1c027e330480c622e3188f99bfb21926744f2bde
tooMofuu0216 <86454385+tooMofuu0216@users.noreply.github.com>
2023-10-16 08:01:06 +0800

    fix nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,56 +14,65 @@ const Navbar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const menuItems = (
         <>
-            <li onClick={onClose}
-                className='hover:text-[#ed6f63] cursor-point'>
-                <Link to="/">Home</Link></li>
-            <li onClick={onClose}
-
+            <Link to="/" onClick={onClose}>
+                <li
+                    onClick={onClose}
+                    className='hover:text-[#ed6f63] cursor-point'>
+                    Home
+                </li>
+            </Link>
+            <Link to="/video" onClick={onClose}>
+                <li
+                    className='hover:text-[#ed6f63] cursor-pointer '>
+                    Video Transcription
+                </li>
+            </Link>
+            <Link
+                to="/docQA" onClick={onClose}>
+            <li
                 className='hover:text-[#ed6f63] cursor-pointer '>
-                <Link to="/video">Video Transcription</Link></li>
-            <li onClick={onClose}
-
-                className='hover:text-[#ed6f63] cursor-pointer '>
-                <Link to="/docQA">Document Q&A</Link></li>
+                Document Q&A
+            </li>
+        </Link>
         </>
     )
 
-    return (
-        <div className="flex justify-between items-center p-4 bg-[#101820]">
-            <div>
+return (
+    <div className="flex justify-between items-center p-4 bg-[#101820]">
+        <div>
 
-            </div>
-            <ul className="sm:flex hidden flex-row space-y-2 sm:space-y-0 space-x-4 px-8 font-semibold">
-                {menuItems}
-            </ul>
-            <div className='sm:hidden'>
-                <button className='text-xl' onClick={onOpen}><AiOutlineMenu /></button>
-                <Drawer
-                    isOpen={isOpen}
-                    placement='right'
-                    onClose={onClose}
+        </div>
+        <ul className="sm:flex hidden flex-row space-y-2 sm:space-y-0 space-x-4 px-8 font-semibold">
+            {menuItems}
+        </ul>
+        <div className='sm:hidden'>
+            <button className='text-xl' onClick={onOpen}><AiOutlineMenu /></button>
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
 
-                // finalFocusRef={btnRef}
+            // finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent
+                    backgroundColor={`#101820`}
+                    color={`white`}
                 >
-                    <DrawerOverlay />
-                    <DrawerContent
-                        backgroundColor={`#101820`}
-                        color={`white`}
-                    >
-                        <DrawerCloseButton />
-                        <DrawerHeader borderBottomWidth='1px'>
-                            AI Playground
-                        </DrawerHeader>
-                        <DrawerBody>
-                            <ul className="flex flex-col space-y-2 sm:space-y-0 ">
-                                {menuItems}
-                            </ul>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
-            </div>
-        </div >
-    )
+                    <DrawerCloseButton />
+                    <DrawerHeader borderBottomWidth='1px'>
+                        AI Playground
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <ul className="flex flex-col space-y-2 sm:space-y-0 ">
+                            {menuItems}
+                        </ul>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </div>
+    </div >
+)
 }
 
 export default Navbar
